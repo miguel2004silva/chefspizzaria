0702a438ce76b491d2b21f9207c01b5893653a7b
Miguel de Sá Silva <133482502+miguel2004silva@users.noreply.github.com>
2025-02-18 00:15:58 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,30 @@
-// Adiciona uma classe ao header quando o usuário rola a página
-window.addEventListener("scroll", function () {
-    let header = document.querySelector(".header");
-    if (window.scrollY > 50) {
-        header.classList.add("scrolled"); // Adiciona a classe quando rolar para baixo
-    } else {
-        header.classList.remove("scrolled"); // Remove a classe quando estiver no topo
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        var header = document.querySelector(".header");
+        var links = document.querySelectorAll(".header section nav a");
+
+        if (window.scrollY > 50) {
+            header.classList.add("scrolled");
+            links.forEach(link => link.style.color = "#131313"); // Cor preta nos links
+        } else {
+            header.classList.remove("scrolled");
+            links.forEach(link => link.style.color = "#fff"); // Volta para branco
+        }
+    });
 });
+
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function mostrarModal(){
+
+    modal.style.left = '50%'
+    mascara.style.visibility = 'visible'
+
+}
+
+function esconderModal(){
+    modal.style.left = '-30%'
+    mascara.style.visibility = 'hidden'
+}
+
